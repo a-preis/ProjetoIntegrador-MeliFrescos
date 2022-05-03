f599aea3894a6fbdb6c8533316553dedb772e25a
a-preis <ana.preis@mercadolivre.com>
2022-05-03 11:57:59 -0300

    fix decimalMax values validations

--- src/main/java/br/com/meli/PIFrescos/models/ProductDimension.java
+++ src/main/java/br/com/meli/PIFrescos/models/ProductDimension.java
@@ -26,17 +26,17 @@ public class ProductDimension {
     private Product product;
 
     @NotNull(message = "Height field can`t be empty")
-    @DecimalMax(value = "200", message = "Height can't be greater than 300 cm.")
+    @DecimalMax(value = "300", message = "Height can't be greater than 300 cm.")
     @DecimalMin(value = "0", message = "Height can't be smaller than 0 cm.")
     private Float height;
 
     @NotNull(message = "Width field can`t be empty")
-    @DecimalMax(value = "200", message = "Width can't be greater than 300 cm.")
+    @DecimalMax(value = "300", message = "Width can't be greater than 300 cm.")
     @DecimalMin(value = "0", message = "Width can't be smaller than 0 cm.")
     private Float width;
 
     @NotNull(message = "Weight field can`t be empty")
-    @DecimalMax(value = "200", message = "Weight can't be greater than 2000 g.")
+    @DecimalMax(value = "2000", message = "Weight can't be greater than 2000 g.")
     @DecimalMin(value = "0", message = "Weight can't be smaller than 0 g.")
     private Float weight;
 }
